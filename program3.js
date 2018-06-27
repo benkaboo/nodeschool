32c32a80bd77e5ba4d66dc14f68ca2d7dd91f1e1
Ben Maslen <ben.maslen@myob.com>
2018-06-27 11:00:41 +1000

    updated error handing

--- program3.js
+++ program3.js
@@ -3,8 +3,11 @@ var fileBuffer = undefined;
 
 fs.readFile(process.argv[2], function doneReading(err, fileBuffer){
 
+if(! err){
   var fileAsString = fileBuffer.toString();
   var splitArray = fileAsString.split('\n');
   console.log(splitArray.length - 1);
-
+}else{
+  console.log('error occured');
+}
 });
